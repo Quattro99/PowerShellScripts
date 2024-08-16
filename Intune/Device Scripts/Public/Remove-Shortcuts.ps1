903240035dad33a73860b9d3f300e32bc24c2d18
Michele Blum <97525515+Quattro99@users.noreply.github.com>
2024-08-16 09:08:06 +0200

    Update Remove-Shortcuts.ps1

--- Intune/Device Scripts/Public/Remove-Shortcuts.ps1
+++ Intune/Device Scripts/Public/Remove-Shortcuts.ps1
@@ -1,26 +1,45 @@
+<#
+.SYNOPSIS
+    Script to remove desktop shortcuts.
+.DESCRIPTION
+    This script deletes the .lnk files (shortcuts) from the desktop of the current user. 
+    It can also delete shortcuts from the public desktop, if indicated.
+.INPUTS
+    None
+.OUTPUTS
+    None
+.NOTES
+   ===========================================================================
+	 Created on:   	16.08.2024
+	 Created by:   	Michele Blum
+	 Filename:     	Remove-Shortcuts.ps1
+	===========================================================================
+.COMPONENT
+    Utility
+.ROLE
+    Maintenance
+.FUNCTIONALITY
+    Cleans up desktop shortcuts.
+#>
+
 function Remove-Shortcuts {
-    <#
-    .DESCRIPTION
-    Removes desktop shortcuts
- 
-    .EXAMPLE
-    Remove-Shortcuts
-    #>
     [CmdletBinding()]
     param (
-        [parameter(Mandatory = $false)]$MyDesktopOnly
+        [parameter(Mandatory = $false, 
+                   HelpMessage = "Specify if you want to delete shortcuts only from current user's desktop.")]
+        [bool]$MyDesktopOnly = $false
     )
 
-    #Check for Admin Privleges
-    Get-Elevation
-
     Write-Host 'Deleting desktop icons...' -ForegroundColor Green
-    if ($MyDesktopOnly) {
-        Remove-Item C:\Users\$env:USERNAME\Desktop\*.lnk -Force -Verbose
-    }
 
-    else {
-        Remove-Item C:\Users\$env:USERNAME\Desktop\*.lnk -Force -Verbose
-        Remove-Item C:\Users\public\Desktop\*.lnk -Force -Verbose
+    # Remove shortcuts from current user's desktop
+    Remove-Item -Path "C:\Users\$env:USERNAME\Desktop\*.lnk" -Force -Verbose
+
+    # If MyDesktopOnly is not specified, also remove from public desktop
+    if (-not $MyDesktopOnly) {
+        Remove-Item -Path "C:\Users\Public\Desktop\*.lnk" -Force -Verbose
     }
 }
+
+# Invoke the function
+Remove-Shortcuts
