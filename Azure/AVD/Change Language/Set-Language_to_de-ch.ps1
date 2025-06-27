1970e588eeffa9e718e06ddf7e38391e13e01796
Michele Blum <97525515+Quattro99@users.noreply.github.com>
2025-06-27 11:47:42 +0200

    Update Set-Language_to_de-ch.ps1

--- Azure/AVD/Change Language/Set-Language_to_de-ch.ps1
+++ Azure/AVD/Change Language/Set-Language_to_de-ch.ps1
@@ -34,7 +34,7 @@ Start-Transcript -Path $logFilePath -Append
 Write-Host "Starting the language and locale configuration to German (Switzerland)..." -ForegroundColor Green
 
 # Define the URL where the XML file is located
-$xmlUrl = "https://raw.githubusercontent.com/Quattro99/PowerShellScripts/5992efa6edb488e7b2a68f50fe504aa14c9f76c6/Azure/AVD/Change%20Language/CHRegion.xml"
+$xmlUrl = "https://raw.githubusercontent.com/Quattro99/PowerShellScripts/refs/heads/main/Azure/AVD/Change%20Language/CHRegion.xml"
 
 # Check if C:\Temp already exists
 $tempDirPath = "C:\Temp"
